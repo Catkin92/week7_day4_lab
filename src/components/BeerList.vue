cc07512f442dfbf9d4f7d8d203b49c6ba1bad1c6
Catrin <catrinhughes92@gmail.com>
2019-11-28 11:00:08 +0000

    beer list structure done

--- src/components/BeerList.vue
+++ src/components/BeerList.vue
@@ -1,10 +1,15 @@
 <template lang="html">
-
+  <div id="beer-list">
+    <ul>
+      <list-component v-for="beer in beers" :beer="beer"></list-component>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  
+  name: 'beer-list',
+  props: ['beers']
 }
 </script>
 
